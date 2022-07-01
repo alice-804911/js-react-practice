d58b506ec223f5b4ad82f04cc77eb689d8276ea1
李方瑜(Alice) <95396221+alice-804911@users.noreply.github.com>
2022-07-01 18:06:33 +0800

    完成靜態切版
    再把keyword tag拆成component
    完成靜態切版

---
+++ src/components/KeywordsTag.js
@@ -0,0 +1,9 @@
+function KeywordsTag() {
+	return (
+		<li className="o-keywords__tag">
+			<a href="http://" alt="tags">#曜越電競椅</a>
+		</li>
+	)
+}
+
+export default KeywordsTag;
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,12 +1,12 @@
 function Pagination() {
 	return (
 		<div className="c-pagination">
-			<div className="c-pagination__icon">
-				<span className="o-icon o-icon--arrowLeft"></span>
+			<div className="c-pagination__icon c-pagination__icon--disable">
+				<span className="o-icon o-icon__arrow o-icon__arrow--left"></span>
 			</div>
-			<div className="c-pagination__pages">1 / 3</div>
+			<div className="c-pagination__pages"><span className="c-pagination__pages--active">1</span> / 3</div>
 			<div className="c-pagination__icon">
-				<span className="o-icon o-icon--arrowRight"></span>
+				<span className="o-icon o-icon__arrow o-icon__arrow--right"></span>
 			</div>
 		</div>
 	)
