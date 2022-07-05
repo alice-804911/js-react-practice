76cb88c720f250242a119c0ea49dd2de8d6a3f02
李方瑜(Alice) <95396221+alice-804911@users.noreply.github.com>
2022-07-05 15:47:31 +0800

    Update Pagination.js
    嘗試加入一個簡單的useSate

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
+
+
+
 function Pagination() {
+	const [page, setPage] = useState(1);
 	return (
 		<div className="c-pagination">
 			<div className="c-pagination__icon c-pagination__icon--disable">
 				<span className="o-icon o-icon__arrow o-icon__arrow--left"></span>
 			</div>
-			<div className="c-pagination__pages"><a href="http://" className="c-pagination__pages--active">1</a> / <a href="http://">3</a></div>
+			<div className="c-pagination__pages"><a href="http://" className="c-pagination__pages--active">{page}</a> / <a href="http://">3</a></div>
 			<div className="c-pagination__icon">
-				<span className="o-icon o-icon__arrow o-icon__arrow--right"></span>
+				<span className="o-icon o-icon__arrow o-icon__arrow--right" onClick={() => setPage(page + 1)}></span>
 			</div>
 		</div>
 	)
