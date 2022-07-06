d6a64db18ca818cb65f84ce81bec00184f0f3296
李方瑜(Alice) <95396221+alice-804911@users.noreply.github.com>
2022-07-06 17:58:12 +0800

    Update ListItem.js
    補上price

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,7 +7,7 @@ function ListItem() {
 				<a href="http://" alt="prodlink">
 					<img src={"https://cs-a.ecimg.tw" + dataRight[1].Img.Src} alt="商品圖" className="o-productInfo__img" />
 					<h3 className="o-productInfo__title">{dataRight[1].Link.Text}</h3>
-					<div className="o-productInfo__price">$1111</div>
+					<div className="o-productInfo__price">{dataRight[3].Link.Text1}</div>
 				</a>
 			</div>
 		</li>
