1eb5adcfccfdaade6ee1ee24b0eaf4eb737f88de
李方瑜(Alice) <95396221+alice-804911@users.noreply.github.com>
2022-07-06 18:29:07 +0800

    Update ListItem.js
    嘗試用.map的方式中

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,19 @@
 import { dataRight } from '../data';
 
+
+// dataRight.map(data => {
+// 	return (
+// 		<li className="c-listGroup__item">
+// 			<div className="o-productInfo">
+// 				<a href="http://" alt="prodlink">
+// 					<img src={`https://cs-a.ecimg.tw${data.Img.src}`} alt="商品圖" />
+// 					<h3 className="o-productInfo__title">{data.Link.Text}</h3>
+// 					<div className="o-productInfo__price">${data.Link.Text1}</div>
+// 				</a>
+// 			</div>
+// 		</li>
+// 	)
+// }
 function ListItem() {
 	return (
 		<li className="c-listGroup__item">
@@ -7,7 +21,7 @@ function ListItem() {
 				<a href="http://" alt="prodlink">
 					<img src={"https://cs-a.ecimg.tw" + dataRight[1].Img.Src} alt="商品圖" className="o-productInfo__img" />
 					<h3 className="o-productInfo__title">{dataRight[1].Link.Text}</h3>
-					<div className="o-productInfo__price">{dataRight[3].Link.Text1}</div>
+					<div className="o-productInfo__price">${dataRight[3].Link.Text1}</div>
 				</a>
 			</div>
 		</li>
