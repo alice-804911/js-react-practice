ca055792a0cb84b257e0a462e3c8661c9b97ae14
李方瑜(Alice) <95396221+alice-804911@users.noreply.github.com>
2022-07-06 12:13:07 +0800

    練習Part.2
    - 刪除原本的theme假data
    - 用工程提供的新假data
    - 把假data分成3包，先拆出第一包並套入tage文字"Link.Text2"
    - 小調整CSS

--- src/components/ThemeGroup.js
+++ src/components/ThemeGroup.js
@@ -2,19 +2,26 @@ import Pagination from './Pagination';
 import ListGroup from './ListGroup';
 import KeywordsTag from './KeywordsTag';
 
-const theme = {
-	'tagName': '主題推薦',
-	'title': '3C 電競年中戰',
-	'img': 'https://picsum.photos/720/1192'
+const dataLeftInfo = {
+	Id: 1,
+	Link: {
+		Text: "真該死",
+		Text2: "好棒棒",
+		Background: "#FF0000"
+	},
+	Img: {
+		Src: "https://fs-a.ecimg.tw/img/h24/v1/layout/onsale/20220704/20220630160437_welcome-2-B-1000x1000.jpg",
+		Text: "test",
+	},
 }
 
 function ThemeGroup() {
 	return (
 		<section className="c-themeGroup">
 			<div className="c-themeGroup__left">
-				<div className="c-themeGroup__tag">{theme.tagName}</div>
+				<div className="c-themeGroup__tag">{dataLeftInfo.Link.Text2}</div>
 				<div className="c-themeGroup__info">
-					<h1 className="c-themeGroup__title">{theme.title}</h1>
+					<h1 className="c-themeGroup__title">aaa</h1>
 					<ul className="o-keywords">
 						<KeywordsTag />
 						<KeywordsTag />
@@ -22,7 +29,7 @@ function ThemeGroup() {
 					</ul>
 				</div>
 				<div className="c-themeGroup__banner">
-					<img src={theme.img} alt="主打推薦Banner" />
+					<img src="" alt="主打推薦Banner" />
 				</div>
 			</div>
 			<div className="c-themeGroup__right">
