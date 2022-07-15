6bc393d950f64aa41ae8826f1ab53515d406f2c2
李方瑜(Alice) <95396221+alice-804911@users.noreply.github.com>
2022-07-15 17:15:17 +0800

    把data獨立出來
    把data獨立出來

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,4 +1,4 @@
-import { dataRight } from '../data';
+// import { dataRight } from '../data';
 
 // function ListItem() {
 // 	return (
@@ -13,37 +13,35 @@ import { dataRight } from '../data';
 // 		</li>
 // 	)
 // }
-function ListItem() {
-	const datalist = dataRight.map((data) => {
-		return (
-			<li className="c-listGroup__item">
-				<div className="o-productInfo">
-					<a href="http://" alt="prodlink">
-						<img src={`https://cs-a.ecimg.tw${data.Img.Src}`} alt="商品圖" className="o-productInfo__img" />
-						<h3 className="o-productInfo__title">{data.Link.Text}</h3>
-						<div className="o-productInfo__price">${data.Link.Text1}</div>
-					</a>
-				</div>
-			</li>
-		)
-	});
-	return datalist;
-}
-
 // function ListItem() {
-// 	dataRight.map(data => {
+// 	const datalist = dataRight.map((data) => {
 // 		return (
 // 			<li className="c-listGroup__item">
 // 				<div className="o-productInfo">
 // 					<a href="http://" alt="prodlink">
-// 						<img src={`https://cs-a.ecimg.tw${data.Img.Src}`} alt="商品圖" />
+// 						<img src={`https://cs-a.ecimg.tw${data.Img.Src}`} alt="商品圖" className="o-productInfo__img" />
 // 						<h3 className="o-productInfo__title">{data.Link.Text}</h3>
 // 						<div className="o-productInfo__price">${data.Link.Text1}</div>
 // 					</a>
 // 				</div>
 // 			</li>
 // 		)
-// 	})
+// 	});
+// 	return datalist;
 // }
+
+function ListItem() {
+	return (
+		<li className="c-listGroup__item">
+			<div className="o-productInfo">
+				<a href="http://" alt="prodlink">
+					<img src="#" alt="商品圖" className="o-productInfo__img" />
+					<h3 className="o-productInfo__title">111</h3>
+					<div className="o-productInfo__price">$</div>
+				</a>
+			</div>
+		</li>
+	)
+}
 
 export default ListItem;
