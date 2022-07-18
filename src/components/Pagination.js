0ace116c489186977a9fc1a5cee699e263d9af8f
李方瑜(Alice) <95396221+alice-804911@users.noreply.github.com>
2022-07-18 17:01:42 +0800

    調整CSS&pagination
    調整c-listGroup
    調整pagination的state

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -11,7 +11,7 @@ function Pagination() {
 			</div>
 			<div className="c-pagination__pages"><a href="http://" className="c-pagination__pages--active">{page}</a> / <a href="http://">3</a></div>
 			<div className="c-pagination__icon">
-				<span className="o-icon o-icon__arrow o-icon__arrow--right" onClick={() => setPage(page + 1)}></span>
+				<span className="o-icon o-icon__arrow o-icon__arrow--right" onClick={() => setPage(page > 2 ? page : page + 1)}></span>
 			</div>
 		</div>
 	)
