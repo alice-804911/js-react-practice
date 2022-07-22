7f7486a482682beaa9525576718bc1d638d42ef5
李方瑜(Alice) <95396221+alice-804911@users.noreply.github.com>
2022-07-22 16:27:36 +0800

    Update Pagination.js
    調整左右pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -4,12 +4,12 @@ function Pagination() {
 	const [page, setPage] = useState(1);
 	return (
 		<div className="c-pagination">
-			<div className="c-pagination__icon c-pagination__icon--disable">
+			<div className="c-pagination__icon" onClick={() => setPage(page < 2 ? page : page - 1)}>
 				<span className="o-icon o-icon__arrow o-icon__arrow--left"></span>
 			</div>
 			<div className="c-pagination__pages"><a href="http://" className="c-pagination__pages--active">{page}</a> / <a href="http://">3</a></div>
-			<div className="c-pagination__icon">
-				<span className="o-icon o-icon__arrow o-icon__arrow--right" onClick={() => setPage(page > 2 ? page : page + 1)}></span>
+			<div className="c-pagination__icon" onClick={() => setPage(page > 2 ? page : page + 1)}>
+				<span className="o-icon o-icon__arrow o-icon__arrow--right"></span>
 			</div>
 		</div>
 	)
